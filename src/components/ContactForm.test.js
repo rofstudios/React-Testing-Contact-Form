d26d1893f35eb8086192fc4be7017268dc273899
Your Name <you@example.com>
2020-03-13 11:00:34 -0400

    no stretch

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -3,5 +3,28 @@ import { render, fireEvent } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 test('if form renders', () => {
-    render(<ContactForm/>);
-})
+    render(<ContactForm />);
+});
+
+test('testing to see if it accepts inputs/submit', () => {
+    let {getByLabelText, findAllByText, getByText} = render(<ContactForm />);
+    
+    //grabbing inputs/submit from form
+
+    let firstName = getByLabelText(/first name/i),
+        lastName = getByLabelText(/last name/i),
+        email = getByLabelText(/email/i);
+    
+    fireEvent.change(firstName, { target: {name: 'firstName', value: 'John'}});
+    fireEvent.change(lastName, {target: {name: 'lastName', value: 'Smith'}});
+    fireEvent.change(email, {target: {name: 'email', value: 'someemail@email.com'}});
+
+    let submit = getByText(/submit/i);
+
+    fireEvent.click(submit);
+
+    // findAllByText(/john/i);
+
+
+
+});
